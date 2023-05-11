b3e8548f082eff1d5ddc0139a82016fa397ddf06
szakeerfu <shaikzakeer202557@gmail.com>
2023-05-11 22:34:00 +0530

    this is tic tot game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useEffect, useState } from 'react';
+import Cell from './components/atom/game/Cell';
+
 function App() {
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""])
+  const [go, setGo] = useState("circle");
+  const [winningMessage, setWinningMaessage] = useState(null);
+
+
+  const message = "it is now" + go + " 's go."
+  console.log(cells)
+
+  const checkScore = () => {
+    const winningCombos = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], /* Hor WINW*/
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], /* VERTICAL WIN*/
+      [0, 4, 8,], [2, 4, 6] /* cros Win*/
+
+    ]
+    winningCombos.forEach(array => {
+      let winningCricle = array.every(cell => cells[cell] === "cricle")
+
+      if (winningCricle) {
+        setWinningMaessage("Cricle wins")
+        return
+
+      }
+
+    })
+
+    winningCombos.forEach(array => {
+      let winningCross = array.every(cell => cells[cell] === "cross")
+
+      if (winningCross) {
+        setWinningMaessage("cross wins")
+        return
+
+      }
+
+    })
+
+  }
+
+
+  useEffect(() => {
+    checkScore();
+  }, [cells])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='gameboard'>
+        {
+          cells.map((cell, index) =>
+            <Cell key={index} id={index}
+              cell={cell}
+              setCells={setCells}
+              go={go}
+              setGo={setGo}
+              cells={cells}
+              winningMessage={winningMessage}
+            />)
+        }
+
+
+      </div>
+      <p>{winningMessage || message}</p>
+
+
+
+
     </div>
   );
 }

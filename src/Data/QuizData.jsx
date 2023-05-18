461ab0b5972ae8994029a518fbf27f6dd993f670
szakeerfu <shaikzakeer202557@gmail.com>
2023-05-18 10:00:17 +0530

    this is master

---
+++ src/Data/QuizData.jsx
@@ -0,0 +1,42 @@
+export const QuizData = [
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+    {
+        question:  "Who developed JavaScript, and what was the first name of JavaScript?",
+        options: ["jackjava", "  Brendan Eich, ", "jonn", "none of the above"],
+        answer:2
+    },
+    {
+        question: "What is BOM?",
+        options: [" BOM stands for Browser  of Modle. ", "   BOM stands for Browser Old Model. ", "BOM stands for Browser  Over Model. ", " BOM stands for Browser Object Model. "],
+        answer:4
+    },
+    {
+        question: "  How to create objects in JavaScript?",
+        options: ["  {} ", "  () ", " // ", "  [[]] "],
+        answer:1
+    },
+    {
+        question: "  What are the pop-up boxes available in JavaScript?",
+        options: ["  Prompt Box ", "  Confirm Box ", "  Alert Box ", " none of above "],
+        answer:3
+    },
+];
